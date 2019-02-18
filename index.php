0e401d4e3dd26bc023a5d4efad5a4f95ec3a735d
Richard Jones <richardnz@outlook.com>
2019-02-18 15:12:08 +1300

    partials

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
-
-require  '../moodle/lib/mustache/src/Mustache/Autoloader.php';
+require 'mustache/src/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
-$m = new Mustache_Engine(['loader' => new
+  $m = new Mustache_Engine(['loader' => new
         Mustache_Loader_FilesystemLoader(dirname(__FILE__) .
         '/templates')]);
 
 $template = $m->loadTemplate('welcomepage');
-echo $template->render(['firstname' => 'John',
-        'visitorNumber' => 7]);
+$data = new stdClass;
+$data->title = 'My registration page';
+$data->body = 'Welcome to the site.';
+$data->copyright = '(c) Richard 2019';
+echo $template->render($data);
 ?>
+</body>
